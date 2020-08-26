6bdb841730e44d79ee5aad9e5e178444a0803623
18790391230 <751586981@qq.com>
2020-08-26 21:31:54 +0800

    1

--- src/test/java/com/wym/guava/BloomFilterTest.java
+++ src/test/java/com/wym/guava/BloomFilterTest.java
@@ -4,7 +4,17 @@ import com.google.common.base.Charsets;
 import com.google.common.hash.BloomFilter;
 import com.google.common.hash.Funnels;
 
-
+/**
+ * 如果布隆过滤器判断一个数据不在过滤器中，则一定不在过滤器中，如果判断在过滤其中，则有很小可能不在过滤器中（存在一定误判率，默认0.03）
+ *
+ * 应用：
+ * 1. 爬虫对URL去重，避免爬取相同URL的地址
+ * 2. 将文章id加入布隆过滤器，避免向用户推荐用户已经读过的文章
+ * 3. 反垃圾邮件，从数十亿垃圾邮件列表中判断某邮箱是否是垃圾邮箱
+ * 4. 将数据主键缓存到布隆过滤器中，当根据ID进行查询时，先通过布隆过滤器判断数据是否存在，如果数据不存在，则直接返回，避免访问数据库，
+ * 可以解决缓存穿透问题
+ * 5. google chrome使用布隆过滤器识别恶意URL
+ */
 public class BloomFilterTest {
 
 
