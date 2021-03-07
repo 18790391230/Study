3462a9c3ebbbb7ab7a454b50a3fcd2e641bd90a3
18790391230 <751586981@qq.com>
2021-03-08 00:32:23 +0800

    User占用内存大小计算，注意name是一个对象

--- src/main/java/com/wym/object/ObjectSize.java
+++ src/main/java/com/wym/object/ObjectSize.java
@@ -18,5 +18,29 @@ public class ObjectSize {
         //     12     4        (loss due to the next object alignment)
         //Instance size: 16 bytes
         //Space losses: 0 bytes internal + 4 bytes external = 4 bytes total//
+
+        System.out.println("===============================\n");
+        User user = new User("张三", 1);
+        System.out.println(ClassLayout.parseInstance(user).toPrintable());
+        //com.wym.object.ObjectSize$User object internals:
+        // OFFSET  SIZE                TYPE DESCRIPTION                               VALUE
+        //      0     4                     (object header)                           01 00 00 00 (00000001 00000000 00000000 00000000) (1)
+        //      4     4                     (object header)                           00 00 00 00 (00000000 00000000 00000000 00000000) (0)
+        //      8     4                     (object header)                           d0 df 00 f8 (11010000 11011111 00000000 11111000) (-134160432)
+        //     12     4    java.lang.String User.name                                 (object)
+        //     16     4   java.lang.Integer User.id                                   1
+        //     20     4                     (loss due to the next object alignment)
+        //Instance size: 24 bytes
+        //Space losses: 0 bytes internal + 4 bytes external = 4 bytes total
+    }
+
+    private static class User{
+        private String name;
+        private Integer id;
+
+        public User(String name, Integer id) {
+            this.name = name;
+            this.id = id;
+        }
     }
 }
